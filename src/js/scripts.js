4cc3143729bb976b800bc5cba0e889640d2fed62
jmaltedo <dohrendorfjm@aol.de>
2022-02-10 21:05:36 +0100

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -11,13 +11,12 @@ document.getElementById("mobile-nav-close-button").onclick = function() {
 // Armenhaus Bildwechsel
 
 document.getElementById("armenhausUD").onclick = function() {
-  document.getElementById("armenhausUD").src = "/kuehsen-dok/src/img/sozialwesen/armenhausUD.jpg";
-  // if (document.getElementById("armenhausUD").src == "/kuehsen-dok/src/img/sozialwesen/armenhausUDsw.jpg")
-  // {
-  //   document.getElementById("armenhausUD").src = "/kuehsen-dok/src/img/sozialwesen/armenhausUD.jpg";
-  // }
-  // else
-  // {
-  //   document.getElementById("armenhausUD").src = "/kuehsen-dok/src/img/sozialwesen/armenhausUDsw.jpg";
-  // }
+  if (document.getElementById("armenhausUD").src == "/kuehsen-dok/src/img/sozialwesen/armenhausUDsw.jpg")
+  {
+    document.getElementById("armenhausUD").src = "/kuehsen-dok/src/img/sozialwesen/armenhausUD.jpg";
+  }
+  else
+  {
+    document.getElementById("armenhausUD").src = "/kuehsen-dok/src/img/sozialwesen/armenhausUDsw.jpg";
+  }
 }
