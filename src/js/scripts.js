5e49f41c6f705f2126212174e9885fe8f92e9f3e
jmaltedo <dohrendorfjm@aol.de>
2022-02-10 20:38:26 +0100

    commit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,3 +7,15 @@ document.getElementById("mobile-nav-button").onclick = function() {
 document.getElementById("mobile-nav-close-button").onclick = function() {
   document.getElementById("mobile-nav").style.right = "-320px";
 }
+
+
+document.getElementById("armenhausUD").onclick = function() {
+  if (document.getElementById("armenhausUD").src == "/kuehsen-dok/src/img/sozialwesen/armenhausUDsw.jpg")
+  {
+    document.getElementById("armenhausUD").src = "/kuehsen-dok/src/img/sozialwesen/armenhausUD.jpg";
+  }
+  else
+  {
+    document.getElementById("armenhausUD").src = "/kuehsen-dok/src/img/sozialwesen/armenhausUDsw.jpg";
+  };
+}
