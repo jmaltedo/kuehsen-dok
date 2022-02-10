ebdac0ab07ef3887b1cbc3ba6f53d66339ea0409
jmaltedo <dohrendorfjm@aol.de>
2022-02-10 21:30:36 +0100

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,7 +15,7 @@ document.getElementById("armenhausUD").onclick = function() {
   {
     document.getElementById("armenhausUD").src = "/kuehsen-dok/src/img/sozialwesen/armenhausUD.jpg";
   }
-  else if
+  else if (document.getElementById("armenhausUD").src == "/kuehsen-dok/src/img/sozialwesen/armenhausUD.jpg")
   {
     document.getElementById("armenhausUD").src = "/kuehsen-dok/src/img/sozialwesen/armenhausUDsw.jpg";
   }
